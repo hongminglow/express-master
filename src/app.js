88e65713c27cec9b0fc7a3c00f8182d65dc88103
ming <hongming_1212@hotmail.com>
2025-10-29 00:01:53 +0800

    feat: finalize all necessary controllers, middleware, and testing

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import authRoutes from '#routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
+import usersRoutes from '#routes/users.routes.js';
 
 const app = express();
 
@@ -14,12 +17,38 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(
-  morgan('combined', { stream: { write: msg => logger.info(msg.trim()) } })
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
 );
 
+app.use(securityMiddleware);
+
 app.get('/', (req, res) => {
-  logger.info('Hello from express master');
-  res.status(200).send('Hello from Acquisitions');
+  logger.info('Hello from Acquisitions!');
+
+  res.status(200).send('Hello from Acquisitions!');
+});
+
+app.get('/health', (req, res) => {
+  res
+    .status(200)
+    .json({
+      status: 'OK',
+      timestamp: new Date().toISOString(),
+      uptime: process.uptime(),
+    });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisitions API is running!' });
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
 });
 
 export default app;
--- src/config/logger.js
+++ src/config/logger.js
@@ -9,15 +9,11 @@ const logger = winston.createLogger({
   ),
   defaultMeta: { service: 'express-master-api' },
   transports: [
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/error.lg', level: 'error' }),
     new winston.transports.File({ filename: 'logs/combined.log' }),
   ],
 });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new winston.transports.Console({
